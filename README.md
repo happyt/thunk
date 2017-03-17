f1543987e7a2b1590bc58b6225b7b0889c0d0abf
Ian <ian@Leos-iMac.local>
2017-03-17 12:46:02 +0000

    working example w delete

---
+++ README.md
@@ -0,0 +1,5 @@
+## thunk
+
+Simple redux example using thunkl, from Matt Stow example, [https://medium.com/@stowball/a-dummys-guide-to-redux-and-thunk-in-react-d8904a7005d3#.9dasdpdkp](https://medium.com/@stowball/a-dummys-guide-to-redux-and-thunk-in-react-d8904a7005d3#.9dasdpdkp)
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import App from './App';
+import './index.css';
+
+const store = configureStore(); // You can also pass in an initialState here
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+  document.getElementById('root')
+);
